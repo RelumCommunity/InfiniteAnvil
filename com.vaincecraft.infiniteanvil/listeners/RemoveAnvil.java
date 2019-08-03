f62f7a6704ff86c1500c4d4d20015385b3aecfd2
VainceCraft <51292267+vaincecraft@users.noreply.github.com>
2019-08-03 18:26:12 +0200

    Update RemoveAnvil.java

--- com.vaincecraft.infiniteanvil/listeners/RemoveAnvil.java
+++ com.vaincecraft.infiniteanvil/listeners/RemoveAnvil.java
@@ -1,3 +1,7 @@
+/**
+ *  @author nato3623
+ */
+
 package com.vaincecraft.infiniteanvil.listeners;
 
 import org.bukkit.Location;
@@ -8,19 +12,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-import com.vaincecraft.infiniteanvil.configurations.Configuration;
 import com.vaincecraft.infiniteanvil.main.Main;
 import com.vaincecraft.infiniteanvil.utils.Data;
 
 public class RemoveAnvil implements Listener {
-	private Configuration configuration = Main.getInstance().getConfiguration();
 	private Data data = Main.getInstance().getData();
 
 	@EventHandler
 	public void removeAnvil(PlayerInteractEvent e) {
+		String prefixs = Main.getInstance().getConfig().getString("settings.Prefix");
+		String prefix = prefixs.replaceAll("&", "§");
+		String alreadyremoved = Main.getLangFile().getString("messages.already-removed");
+		String colormsg1 = alreadyremoved.replaceAll("&", "§");
+		String nopermremove = Main.getLangFile().getString("messages.no-perm-remove");
+		String colormsg2 = nopermremove.replaceAll("&", "§");
+		String anvilremoves = Main.getLangFile().getString("messages.anvil-remove");
+		String anvilremove = anvilremoves.replaceAll("&", "§"); //colormsg3
 		if (!e.isCancelled()) {
 			Player p = e.getPlayer();
-			if ((e.getAction() == Action.LEFT_CLICK_BLOCK) && (p.isSneaking()) && (e.getClickedBlock().getType() == Material.ANVIL) && (configuration.shiftClick())) {
+			if ((e.getAction() == Action.LEFT_CLICK_BLOCK) && (p.isSneaking()) && (e.getClickedBlock().getType() == Material.ANVIL) && (Main.getInstance().getConfig().getBoolean("settings.shift-click"))) {
 				if (p.hasPermission("infiniteanvil.remove")) {
 					e.setCancelled(true);
 					if (data.checkData(e.getClickedBlock())) {
@@ -31,17 +41,18 @@ public class RemoveAnvil implements Listener {
           
 						Main.getInstance().getData().getLoadData().set(data.getUUID(), null);
 						Main.getInstance().getData().saveData();
-						if (configuration.notifyMessages()) {
-							configuration.anvilRemoveMessage(p, x, y, z);
+						if (Main.getInstance().getConfig().getBoolean("settings.notify")) {
+		    				String colormsg3 = anvilremove.replace("%x%", Integer.toString(x)).replace("%y%", Integer.toString(y)).replace("%z%", Integer.toString(z));
+		    				p.getPlayer().sendMessage(prefix + colormsg3);
 						}
 						return;
 					}
-					if (configuration.notifyMessages()) {
-						configuration.alreadyRemovedMessage(p);
+					if (Main.getInstance().getConfig().getBoolean("settings.notify")) {
+						p.getPlayer().sendMessage(prefix + colormsg1);
 					}
 					return;
 				}
-				configuration.noPermRemoveMessage(p);
+				p.getPlayer().sendMessage(prefix + colormsg2);
 			}
 		}
 	}
