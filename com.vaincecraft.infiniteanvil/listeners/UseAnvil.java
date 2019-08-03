0fa50fb406183f6127de82a0f93274ec5cfdf15c
VainceCraft <51292267+vaincecraft@users.noreply.github.com>
2019-08-03 18:26:40 +0200

    Update UseAnvil.java

--- com.vaincecraft.infiniteanvil/listeners/UseAnvil.java
+++ com.vaincecraft.infiniteanvil/listeners/UseAnvil.java
@@ -8,24 +8,26 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-import com.vaincecraft.infiniteanvil.configurations.Configuration;
 import com.vaincecraft.infiniteanvil.main.Main;
 import com.vaincecraft.infiniteanvil.utils.Data;
 
 public class UseAnvil implements Listener {
-	private Configuration configuration = Main.getInstance().getConfiguration();
 	private Data data = Main.getInstance().getData();
 
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void useAnvil(PlayerInteractEvent e) {
 		if (!e.isCancelled()) {
+			String prefixs = Main.getInstance().getConfig().getString("settings.Prefix");
+			String prefix = prefixs.replaceAll("&", "§");
+			String nopermuse = Main.getLangFile().getString("messages.no-perm-use");
+			String colormsg1 = nopermuse.replaceAll("&", "§");
 			Player p = e.getPlayer();
 			if ((e.getAction() == Action.RIGHT_CLICK_BLOCK) && (e.getClickedBlock().getType() == Material.ANVIL) && (data.checkData(e.getClickedBlock()))) {
 				Location l = e.getClickedBlock().getLocation();
 				if (!p.hasPermission("infiniteanvil.use")) {
 					e.setCancelled(true);
-					configuration.noPermUseMessage(p);
+					p.getPlayer().sendMessage(prefix + colormsg1);
 				}
 				if (e.getClickedBlock().getData() != data.getBlockData().byteValue()) {
 					e.getClickedBlock().getWorld().getBlockAt(l).setData(data.getBlockData().byteValue());
