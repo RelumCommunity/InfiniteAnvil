e244d89a9ea71669bc633e49f955dc38dfd47b26
VainceCraft <51292267+vaincecraft@users.noreply.github.com>
2019-08-03 18:27:10 +0200

    Update Main.java

--- com.vaincecraft.infiniteanvil/main/Main.java
+++ com.vaincecraft.infiniteanvil/main/Main.java
@@ -8,11 +8,11 @@
 package com.vaincecraft.infiniteanvil.main;
 
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.vaincecraft.infiniteanvil.commands.CommandHandler;
-import com.vaincecraft.infiniteanvil.configurations.Configuration;
+import com.vaincecraft.infiniteanvil.crafting.RemoveAnvilCrafting;
 import com.vaincecraft.infiniteanvil.listeners.BreakAnvil;
 import com.vaincecraft.infiniteanvil.listeners.PlaceAnvil;
 import com.vaincecraft.infiniteanvil.listeners.RemoveAnvil;
@@ -20,16 +20,17 @@ import com.vaincecraft.infiniteanvil.listeners.SetAnvil;
 import com.vaincecraft.infiniteanvil.listeners.UseAnvil;
 import com.vaincecraft.infiniteanvil.utils.Data;
 import com.vaincecraft.infiniteanvil.utils.GenerateUUID;
+import com.vaincecraft.infiniteanvil.messages.LanguageFile;
+import com.vaincecraft.infiniteanvil.messages.messages;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin {
 	public static Main plugin;
 	public Data data;
-	public Configuration configuration;
 	public GenerateUUID generateUUID;
 	public CommandHandler commandHandler;
-	public String pluginVersion = "V.1.0";
+	public String pluginVersion = "V.1.2";
 	
 	public void onEnable() {
 		plugin = this;
@@ -46,21 +47,24 @@ public class Main extends JavaPlugin {
 		plugin.getServer().getConsoleSender().sendMessage("[InfiniteAnvil] " + ChatColor.GREEN + "InfiniteAnvil has been enabled (" + pluginVersion + ")");
 		
 		data = new Data();
-		configuration = new Configuration();
 		generateUUID = new GenerateUUID();
 		commandHandler = new CommandHandler();
 		
 		Bukkit.getPluginManager().registerEvents(new VersionChecker(), this);
+		Bukkit.getPluginManager().registerEvents(new RemoveAnvilCrafting(), this);
 		getServer().getPluginManager().registerEvents(new BreakAnvil(), this);
 		getServer().getPluginManager().registerEvents(new PlaceAnvil(), this);
 		getServer().getPluginManager().registerEvents(new RemoveAnvil(), this);
-	  getServer().getPluginManager().registerEvents(new SetAnvil(), this);
-	  getServer().getPluginManager().registerEvents(new UseAnvil(), this);
-	  getCommand("anvil").setExecutor(new CommandHandler());
+	    getServer().getPluginManager().registerEvents(new SetAnvil(), this);
+	    getServer().getPluginManager().registerEvents(new UseAnvil(), this);
+	    getCommand("anvil").setExecutor(new CommandHandler());
 	    
 		data.setup();
 		
 		saveDefaultConfig();
+		
+		new LanguageFile();
+		new messages();
 	}
 	public static Main getInstance() {
 		return plugin;
@@ -68,19 +72,21 @@ public class Main extends JavaPlugin {
 	public Data getData() {
 		return data;
 	}
-	public Configuration getConfiguration() {
-		return configuration;
-	}
 	public GenerateUUID getGenerateUUID() {
 		return generateUUID;
 	}
 	public CommandHandler getCommandHandler() {
 		return commandHandler;
 	}
-	public void onDenyBreak(CommandSender sender) {
-		configuration.noPermBreakMessage(sender);
-	}
 	public void onDisable() {
 		  plugin.getServer().getConsoleSender().sendMessage("[InfiniteAnvil] " + ChatColor.RED + "InfiniteAnvil has been disabled (" + pluginVersion + ")");
-	  }
+	}
+	public static FileConfiguration getLangFile() {
+		messages lang =  new messages();
+		return lang.getFile();
+	}
+	public static String getLang() {
+		String Lang = Main.getInstance().getConfig().getString("settings.Language");
+		return Lang;
+	}
 }
