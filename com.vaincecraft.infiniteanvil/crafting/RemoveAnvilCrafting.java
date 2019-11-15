041669a39e33e8ac8616281ac934a42a0321ad42
VainceCraft <51292267+vaincecraft@users.noreply.github.com>
2019-11-15 19:21:34 +0100

    Update RemoveAnvilCrafting.java

--- com.vaincecraft.infiniteanvil/crafting/RemoveAnvilCrafting.java
+++ com.vaincecraft.infiniteanvil/crafting/RemoveAnvilCrafting.java
@@ -17,10 +17,27 @@ import com.vaincecraft.infiniteanvil.main.Main;
 
 public class RemoveAnvilCrafting implements Listener{
 	@EventHandler
-	public void noAnvilCrafting(PrepareItemCraftEvent e) {
+	public void noAnvilCrafting(PrepareItemCraftEvent l) {
 		if (Main.getInstance().getConfig().getBoolean("settings.RemoveAnvilRecipe")) {
-			if(e.getRecipe().getResult().getType() == Material.ANVIL) {
-				e.getInventory().setResult(new ItemStack(Material.AIR));
+			ItemStack[] matrix = l.getInventory().getMatrix();
+			if (matrix[0].getType().equals(Material.IRON_BLOCK)) {
+				if (matrix[1].getType().equals(Material.IRON_BLOCK)) {
+					if (matrix[2].getType().equals(Material.IRON_BLOCK)) {
+						if (matrix[3].getType().equals(Material.AIR)) {
+							if (matrix[4].getType().equals(Material.IRON_INGOT)) {
+								if (matrix[5].getType().equals(Material.AIR)) {
+									if (matrix[6].getType().equals(Material.IRON_INGOT)) {
+										if (matrix[7].getType().equals(Material.IRON_INGOT)) {
+											if (matrix[8].getType().equals(Material.IRON_INGOT)) {
+												l.getInventory().setResult(new ItemStack(Material.AIR));
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
