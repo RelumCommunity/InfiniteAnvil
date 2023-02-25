9627ea768a35e523042343abc71e668b7e403209
RelumCommunity <51292267+RelumCommunity@users.noreply.github.com>
2023-02-25 12:02:17 +0100

    Create UseAnvil.java

---
+++ com.relumcommunity.com/listeners/UseAnvil.java
@@ -0,0 +1,38 @@
+package com.relumcommunity.infiniteanvil.listeners;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import com.relumcommunity.infiniteanvil.main.Main;
+import com.relumcommunity.infiniteanvil.utils.Data;
+
+public class UseAnvil implements Listener {
+	private Data data = Main.getInstance().getData();
+
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void useAnvil(PlayerInteractEvent e) {
+		if (!e.isCancelled()) {
+			String prefixs = Main.getInstance().getConfig().getString("settings.Prefix");
+			String prefix = prefixs.replaceAll("&", "§");
+			String nopermuse = Main.getLangFile().getString("messages.no-perm-use");
+			String colormsg1 = nopermuse.replaceAll("&", "§");
+			Player p = e.getPlayer();
+			if ((e.getAction() == Action.RIGHT_CLICK_BLOCK) && (e.getClickedBlock().getType() == Material.ANVIL) && (data.checkData(e.getClickedBlock()))) {
+				Location l = e.getClickedBlock().getLocation();
+				if (!p.hasPermission("infiniteanvil.use")) {
+					e.setCancelled(true);
+					p.getPlayer().sendMessage(prefix + colormsg1);
+				}
+				if (e.getClickedBlock().getData() != data.getBlockData().byteValue()) {
+					e.getClickedBlock().getWorld().getBlockAt(l).setData(data.getBlockData().byteValue());
+				}
+			}
+		}
+	}
+}
